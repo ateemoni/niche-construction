1abf7f76041b17b94f75a8a67f804c817f6a7a9e
emoniates <your_email@example.com>
2026-06-14 23:18:01 +0300

    fix children type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import './globals.css'
+import { ReactNode } from 'react'
 
 export const metadata = {
   title: 'Niche Construction',
   description: 'Project Manager',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
